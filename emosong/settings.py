ebcb0267d94d423a6470dc63e57d4e4b5583cc09
devblin <deepanshu.dhruw@outlook.com>
2021-01-29 11:00:22 +0530

    resolved GET error

--- emosong/settings.py
+++ emosong/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('PROD_HOST')]
+ALLOWED_HOSTS = ['127.0.0.1', os.getenv('PROD_HOST')]
 
 
 # Application definition
