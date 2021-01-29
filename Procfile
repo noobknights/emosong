021a4e1326102c6de106bace03328f15739ac08c
devblin <deepanshu.dhruw@outlook.com>
2021-01-29 13:33:19 +0530

    procfile update, added channel layers

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT app:app
 web: gunicorn emosong.wsgi
+web: daphne emosong.asgi:application --port $PORT --bind 0.0.0.0
--- emosong/asgi.py
+++ emosong/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
+# from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emosong.settings')
-
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
